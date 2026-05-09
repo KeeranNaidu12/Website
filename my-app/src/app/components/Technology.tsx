e2155e4943e22ea5018e72cd1d09cbf6f8cb7ad5
KeeranNaidu12 <keerannaidu08@gmail.com>
2026-05-08 17:03:17 -0700

    More technologies

--- my-app/src/app/components/Technology.tsx
+++ my-app/src/app/components/Technology.tsx
@@ -8,8 +8,18 @@ type TechnologyProps ={
 export default function Technology({name,icon} : TechnologyProps){
     return (
       <div className="flex flex-col items-center gap-2 m-10">
-        <img src={icon} alt={name} className="w-16 h-16" />
-        <h1>{name}</h1>
+        <div className="relative group">
+          <div
+            role="tooltip"
+            className="pointer-events-none absolute -top-10 left-1/2 -translate-x-1/2 whitespace-nowrap rounded bg-black px-2 py-1 text-sm text-white opacity-0 transition-opacity duration-150 group-hover:opacity-100"
+          >
+            {name}
+          </div>
+
+          <div className="p-10 m-5 rounded-2xl bg-gray-900 transition-colors duration-150 group-hover:bg-gray-700 cursor-pointer">
+            <img src={icon} alt={name} className="w-16 h-16" />
+          </div>
+        </div>
       </div>
     );
 }
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -8,6 +8,13 @@ const technologies = [
   { name: "PHP", icon: "/icons/PHP.svg" },
   { name: "HTML", icon: "/icons/HTML.svg" },
   { name: "CSS", icon: "/icons/CSS.svg" },
+  { name: "React", icon: "/icons/React.svg" },
+  { name: "Next.js", icon: "/icons/Next.js.svg" },
+  { name: "Node.js", icon: "/icons/Node.js.svg" },
+  { name: "Express.js", icon: "/icons/Express.js.svg" },
+  { name: "Django", icon: "/icons/Django.svg" },
+  { name: "FastAPI", icon: "/icons/FastAPI.svg" },
+  { name: "Spring Boot", icon: "/icons/Spring Boot.svg" },
 ];
 
 export default function Home() {
@@ -16,7 +23,7 @@ export default function Home() {
       <main>
         <section
           id="about"
-          className="bg-slate-950 text-slate-100 min-h-[80vh]"
+          className="bg-slate-950 text-slate-100 min-h-[40vh]"
         >
           <div className="max-w-6xl mx-auto flex items-center gap-10 px-10 py-20">
             <div className="max-w-2xl">
@@ -46,7 +53,7 @@ export default function Home() {
           <h1 className="p-10 text-4xl font-bold text-center text-sky-400">Technologies I have worked with</h1>
           <div className="max-w-6xl mx-auto flex items-center gap-10 px-10 py-20">
         
-            <div className="max-w-2xl">
+            <div className="flex flex-wrap justify-center itmes-center gap-4">
               {technologies.map((t) => (
                 <Technology key={t.name} name={t.name} icon={t.icon} />
               ))}
