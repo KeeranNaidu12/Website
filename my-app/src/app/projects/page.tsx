856bdd652a20911ea52a2f41dd3dc0077d72d5a6
KeeranNaidu12 <keerannaidu08@gmail.com>
2026-05-09 23:23:52 -0700

    Adding projects

--- my-app/src/app/projects/page.tsx
+++ my-app/src/app/projects/page.tsx
@@ -18,31 +18,38 @@ export default function Projects() {
                 name="NuTeach"
                 image="/project_icons/Plataboom.png"
                 description="A local co-op 2D shooter built in Unity and C#, inspired by Pong but with characters that fire projectiles instead of paddles. Across 4 rounds, players race to destroy each other's target on stages packed with gimmicks like moving platforms and spikes, backed by expressive 2D art and chaotic sound design."
-                date="March-April 2025"
-                link="https://battlebee12.itch.io/plata-boom"
+                date="Janaury 2026 - Present"
               />
               <Project
                 name="Personal Website"
                 image="/project_icons/Plataboom.png"
-                description="A local co-op 2D shooter built in Unity and C#, inspired by Pong but with characters that fire projectiles instead of paddles. Across 4 rounds, players race to destroy each other's target on stages packed with gimmicks like moving platforms and spikes, backed by expressive 2D art and chaotic sound design."
-                date="March-April 2025"
-                link="https://battlebee12.itch.io/plata-boom"
+                description="My personal portfolio site that you're currently viewing to showcase my projects and skills. Designed with a modern layout and reusable components, it's continuously updated as I take on new projects"
+                date="May 2025 - Present"
+                github="https://github.com/KeeranNaidu12/Website"
+                link="https://keerandev.com"
               />
               <Project
-                name=""
+                name="Examinator"
                 image="/project_icons/Plataboom.png"
-                description="A local co-op 2D shooter built in Unity and C#, inspired by Pong but with characters that fire projectiles instead of paddles. Across 4 rounds, players race to destroy each other's target on stages packed with gimmicks like moving platforms and spikes, backed by expressive 2D art and chaotic sound design."
-                date="March-April 2025"
-                link="https://battlebee12.itch.io/plata-boom"
+                description="Built a dynamic exam generation and analysis platform that creates unique exam variants by randomizing question types, difficulty levels, topic distribution and mandatory content requirements. Integrated real-time analysis to detect collusion and cheating patterns through statistical modeling, improving academic integrity while maintaining assessment validity across all generated variants."
+                date="May 2025 - August 2025"
               />
               <Project
                 name="Plata-Boom"
                 image="/project_icons/Plataboom.png"
                 description="A local co-op 2D shooter built in Unity and C#, inspired by Pong but with characters that fire projectiles instead of paddles. Across 4 rounds, players race to destroy each other's target on stages packed with gimmicks like moving platforms and spikes, backed by expressive 2D art and chaotic sound design."
-                date="March-April 2025"
+                date="March 2025 - April 2025"
+                github="https://github.com/Battlebee12/416-Game-Jam"
                 link="https://battlebee12.itch.io/plata-boom"
               />
-          </div>
+              <Project
+                name="Jurassic Care"
+                image="/project_icons/JurassicCare.png"
+                description="Jurassic-Care is a dynamic web application simulating an online dinosaur adoption store. Users can register, log in, browse products (dinosaurs), add them to a cart, leave reviews, and complete checkouts. An admin portal supports product management and user moderation."
+                date="January 2025 - April 2025"
+                github="https://github.com/ThunderIW/COSC360_final-Project"
+              />
+            </div>
           </div>
         </section>
       </main>
