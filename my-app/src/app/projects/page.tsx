2e5d52ed021dde1784503cf2010b5a50c21d6cd2
KeeranNaidu12 <keerannaidu08@gmail.com>
2026-05-16 11:39:57 -0700

    Added project

--- my-app/src/app/projects/page.tsx
+++ my-app/src/app/projects/page.tsx
@@ -21,21 +21,29 @@ export default function Projects() {
                 date="May 2026 - Present"
                 github="https://github.com/KeeranNaidu12/Website"
                 link="https://keerandev.com"
-                tech={["TypeScript", "React", "Next.js","Vercel"]}
+                tech={["TypeScript", "React", "Next.js", "Vercel"]}
               />
               <Project
                 name="NuTeach"
                 image="/project_icons/NuTeach.png"
                 description="As part of my Directed Studies course, I worked on gamification software under the supervision of Dr Abdallah Mohamed. My contributions included building UI dashboards, implementing purchase approval functionality, weight-shift rewards, a refunding policy, and user guides."
                 date="Janaury 2026 - Present"
-                tech={["TypeScript", "React", "Node.js","SQL","MariaDB"]}
+                tech={["TypeScript", "React", "Node.js", "SQL", "MariaDB"]}
+              />
+              <Project
+                name="Toronto Crime Analysis"
+                image=""
+                description="Analyzed the crime for Toronto neighbours. Created safety scores to rank neighbourhoods and understand which kinds of crime occur in specific neighbourhoods. Additionally, created LightGBM models to predict assault, auto theft, break and enter and traffic incidents. "
+                date="March 2026 - April 2026"
+                github="https://github.com/KeeranNaidu12/CrimeAnalysis.git"
+                tech={["Python","PostgreSQL", "Power BI"]}
               />
               <Project
                 name="Examinator"
                 image="/project_icons/Examinator.png"
                 description="As part of my capstone course, I built a dynamic exam generation and analysis platform that creates unique exam variants by randomizing question types, difficulty levels, topic distribution and mandatory content requirements. Integrated real-time analysis to detect collusion and cheating patterns through statistical modeling, improving academic integrity while maintaining assessment validity across all generated variants."
                 date="May 2025 - August 2025"
-                tech={["Java", "JavaScript","React","SpringBoot","MySQL"]}
+                tech={["Java", "JavaScript", "React", "SpringBoot", "MySQL"]}
               />
               <Project
                 name="Plata-Boom"
@@ -52,7 +60,7 @@ export default function Projects() {
                 description="Jurassic-Care is a dynamic web application simulating an online dinosaur adoption store. Users can register, log in, browse products (dinosaurs), add them to a cart, leave reviews, and complete checkouts. An admin portal supports product management and user moderation."
                 date="January 2025 - April 2025"
                 github="https://github.com/ThunderIW/COSC360_final-Project"
-                tech={["PHP","JavaScript", "HTML","CSS","SQL"]}
+                tech={["PHP", "JavaScript", "HTML", "CSS", "SQL"]}
               />
             </div>
           </div>
